c2ebf8b71cd1a12e17d9d726a12fff649b9b50f0
MEDICLEMDI <medicle@medicle.io>
2023-02-13 19:06:39 +0900

    페이지 추가, 하단 네비게이션 영역 버그 수정

--- src/components/ImageSlide/index.tsx
+++ src/components/ImageSlide/index.tsx
@@ -1,13 +1,34 @@
-import { View, Image } from "react-native";
+import { Image, Text, View } from 'react-native';
+
+import ImageSlide_1 from '@/assets/images/ImageSlide_1.png';
+
 import style from './style';
+import { fontStyleCreator } from "@/utils/fonts";
+import { Colors } from "@/constants/theme";
 
 const ImageSlide = () => {
-
-    return (
-        <View>
-            <View style={style.imageItem}></View>
-        </View>
-    )
-}
+  const SLIDE_IMAGE_LENGTH_FONT = fontStyleCreator({
+    color: Colors.Medicle.Font.Gray.Light,
+    size: 11,
+  });
+  const SLIDE_IMAGE_INDEX_FONT = fontStyleCreator({
+    color: Colors.Medicle.Font.White,
+    size: 11,
+  });
+  return (
+    <View>
+      {/*<View style={style.imageItem} />*/}
+      <Image
+        source={ImageSlide_1}
+        resizeMode="contain"
+        style={style.imageItem}
+      />
+      <View style={style.imageSlideIndex}>
+        <Text style={SLIDE_IMAGE_INDEX_FONT}>{1}</Text>
+        <Text style={SLIDE_IMAGE_LENGTH_FONT}> / {1}</Text>
+      </View>
+    </View>
+  );
+};
 
 export default ImageSlide;
--- src/screens/service/Point/index.tsx
+++ src/screens/service/Point/index.tsx
@@ -1,26 +1,74 @@
 import { useIsFocused } from '@react-navigation/native';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { FlatList, Platform, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
+import BoxDropShadow from '@/components/BoxDropShadow';
 import Header from '@/components/Header';
+import { Colors } from '@/constants/theme';
 import Icons from '@/icons';
 
 import style from './style';
+import { fontStyleCreator } from "@/utils/fonts";
 
 export default () => {
   const { t } = useTranslation();
   const isFocus = useIsFocused();
 
+  const FONT_BASIC_BLACK = fontStyleCreator({
+    color: Colors.Medicle.Font.Gray.Dark,
+    size: 10,
+  });
+
   return (
     <SafeAreaView style={style.container}>
-      <Header goBack={true} />
-      <ScrollView horizontal={false}>
-        <View style={style.listWrap}>
-          <Icons name="refresh" />
-          <Text>{t('comingSoon')}</Text>
+      <Header goBack={true} title={t('menus.point')} />
+      <BoxDropShadow
+        color={
+          Platform.OS === 'ios'
+            ? Colors.Medicle.Gray.SemiLight
+            : Colors.Medicle.Gray.Standard
+        }
+        offset={[0, 7]}
+        elevation={10}
+        opacity={0.95}
+        radius={10}
+        style={[style.pointWrap]}>
+        <View style={[style.flexRow]}>
+          <Text>메디클 포인트</Text>
+          <TouchableOpacity style={style.infoButton}>
+            <Text style={FONT_BASIC_BLACK}>이용안내</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
+        <View style={[style.flexRow, { marginVertical: 15 }]}>
+          <Icons name="mdiIcon" />
+          <Text style={[style.point]}>{0}원</Text>
+        </View>
+        <Text style={style.pointText}>적립 예정 포인트</Text>
+        <View style={[style.flexRow, { justifyContent: 'space-between' }]}>
+          <Text>{0}원</Text>
+          <TouchableOpacity style={style.chargeButton}>
+            <Text style={FONT_BASIC_BLACK}>충전하기</Text>
+          </TouchableOpacity>
+        </View>
+      </BoxDropShadow>
+      <View style={style.historyWrap}>
+        <View style={[style.searchBar]}>
+          <View style={style.flexRow}>
+            <Text>거래내역</Text>
+            <Text>최근 1년</Text>
+          </View>
+          <TouchableOpacity>
+            <Icons name="arrowDown" />
+          </TouchableOpacity>
+        </View>
+        {/*<FlatList data={} renderItem={}>*/}
+
+        {/*</FlatList>*/}
+        <View style={style.histories}>
+          <Text>사용 포인트 내역이 없습니다.</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
