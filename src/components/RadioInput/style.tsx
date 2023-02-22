4d0cd7c02e323cf78dcb5a6abecf91344995dfe4
MEDICLEMDI <medicle@medicle.io>
2023-02-22 15:28:20 +0900

    Radio Input 컴포넌트 추가

---
+++ src/components/RadioInput/style.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+  radioCircleWrap = {
+    width: size,
+    height: size,
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: borderColor,
+    borderRadius: 100,
+  }
+  radioCircle: {
+    flex: 1,
+    margin: padding,
+    backgroundColor: circleColor,
+    borderRadius: 100,
+  },
+  radioWrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
