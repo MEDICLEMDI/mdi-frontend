f5e091be7fd68b26f490dc79b626242839dd888d
MEDICLEMDI <medicle@medicle.io>
2023-02-23 17:37:15 +0900

    니모닉페이지생성

---
+++ src/screens/service/Wallet/components/Nmemonic/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  titleContainer: {
+    marginTop: 20,
+    height: 27,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#333333',
+  },
+});
