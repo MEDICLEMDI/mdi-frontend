ea7b7333705c0e9ebe2e7e1ff60f471c2b64fbbb
MEDICLEMDI <medicle@medicle.io>
2023-02-27 16:26:54 +0900

    Medicle Input update

--- src/components/inputs/MedicleInput/index.tsx
+++ src/components/inputs/MedicleInput/index.tsx
@@ -2,17 +2,20 @@ import * as React from 'react';
 import { Text, TextInput, TextInputProps, View, ViewStyle } from 'react-native';
 import { Colors } from '@/constants/theme';
 import defaultStyle from './style';
+import {fontStyleCreator} from "@/utils/fonts";
 interface TopLabelInputProps extends TextInputProps {
   readonly direction?: 'row' | 'column' | undefined;
   label?: string | undefined;
   style?: ViewStyle | ViewStyle[];
   password?: boolean;
   errText?: string;
-  iconLeft?: React.ReactNode;
-  iconRight?: React.ReactNode;
+  leftInputNode?: React.ReactNode;
+  rightInputNode?: React.ReactNode;
+  inputButtonNode?: React.ReactNode;
+  marginBottom?: number;
 }
 const MedicleInput = ({
-  direction,
+  direction = 'column',
   label,
   style,
   placeholder,
@@ -26,50 +29,71 @@ const MedicleInput = ({
   value,
   errText,
   maxLength,
-  iconLeft,
-  iconRight,
+  leftInputNode,
+  rightInputNode,
+  inputButtonNode,
+  marginBottom = 0,
 }: TopLabelInputProps) => {
-  const flexDirection = {
-    flexDirection: direction === undefined ? 'column' : direction,
-    alignItem: 'center',
+
+  const ERROR_TEXT = fontStyleCreator({
+    color: '#FF2D2D',
+    size: 12,
+    weight: 'normal',
+  })
+  const [errorMargin, setErrorMargin] = React.useState(0);
+  const flexStyle: ViewStyle = {
+    flexDirection: direction,
+    alignItems: direction === 'row' ? 'center' : 'baseline',
   };
+
+  const onLabelLayout = event => {
+    const { width } = event.nativeEvent.layout;
+    setErrorMargin(width);
+  }
+
   return (
-    <View style={[flexDirection, style]}>
-      {label && <Text>{label}</Text>}
-      <View style={[
-        defaultStyle.inputWrapFlex,
-        { backgroundColor: errText ? '#FFE8E8' : Colors.Medicle.Gray.Light },
-      ]}>
-        { !multiline && iconLeft && iconLeft}
-        <TextInput
-          style={[
-            defaultStyle.inputStyle,
-            multiline ? defaultStyle.textArea : null,
-            { backgroundColor: errText ? '#FFE8E8' : Colors.Medicle.Gray.Light },
-          ]}
-          placeholder={placeholder}
-          maxLength={maxLength}
-          editable={editable}
-          multiline={multiline}
-          numberOfLines={numberOfLines}
-          onChange={onChange}
-          onPressIn={onPressIn}
-          onChangeText={onChangeText}
-          secureTextEntry={password}
-          value={value}
-          // ios settings
-          clearButtonMode="always"
-          enablesReturnKeyAutomatically={true}
-          // android settings
-          disableFullscreenUI={true}
-        />
-        {!multiline && iconRight && iconRight}
+    <View style={{ marginBottom: marginBottom }}>
+      <View style={[flexStyle, defaultStyle.inputWrap]}>
+        {label && (
+          <Text
+            onLayout={onLabelLayout}
+            style={direction === 'row' ? { paddingRight: 10 } : { marginBottom: 10 }}>
+            {label}
+          </Text>
+        )}
+        <View style={[
+          defaultStyle.inputContainer,
+          direction === 'row' && defaultStyle.inputRowDirection,
+          multiline ? defaultStyle.textArea : null,
+          { backgroundColor: errText ? '#FFE8E8' : Colors.Medicle.Gray.Light }]}>
+          {!multiline && leftInputNode && <View>{leftInputNode}</View>}
+          <TextInput
+            style={[
+              defaultStyle.input,
+              direction === 'row' && { flex: 1 },
+              multiline ? defaultStyle.textArea : null
+            ]}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            editable={editable}
+            multiline={multiline}
+            numberOfLines={numberOfLines}
+            onChange={onChange}
+            onPressIn={onPressIn}
+            onChangeText={onChangeText}
+            secureTextEntry={password}
+            value={value}
+            // ios settings
+            clearButtonMode="always"
+            enablesReturnKeyAutomatically={true}
+            // android settings
+            disableFullscreenUI={true}
+          />
+          {!multiline && rightInputNode && <View>{rightInputNode}</View>}
+        </View>
+        {!multiline && inputButtonNode && <View style={{ marginLeft: 10 }}>{inputButtonNode}</View>}
       </View>
-      {errText ? (
-        <Text style={defaultStyle.errText}>{errText}</Text>
-      ) : (
-        <Text style={defaultStyle.errText}>&nbsp;</Text>
-      )}
+      {errText && (<Text style={[ERROR_TEXT, direction === 'row' && {marginLeft: errorMargin}]}>{errText}</Text>)}
     </View>
   );
 };
