dfdc32ef31d803908ffa6602897893924d444c7d
MEDICLEMDI <medicle@medicle.io>
2023-02-27 11:29:40 +0900

    input update

--- src/components/inputs/MedicleInput/index.tsx
+++ src/components/inputs/MedicleInput/index.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { Text, TextInput, TextInputProps, View, ViewStyle } from 'react-native';
-
+import { Colors } from '@/constants/theme';
 import defaultStyle from './style';
-
 interface TopLabelInputProps extends TextInputProps {
   readonly direction?: 'row' | 'column' | undefined;
   label?: string | undefined;
   style?: ViewStyle | ViewStyle[];
   password?: boolean;
+  errText?: string;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
 }
-
 const MedicleInput = ({
   direction,
   label,
@@ -23,34 +24,53 @@ const MedicleInput = ({
   password,
   onChangeText,
   value,
+  errText,
+  maxLength,
+  iconLeft,
+  iconRight,
 }: TopLabelInputProps) => {
   const flexDirection = {
     flexDirection: direction === undefined ? 'column' : direction,
     alignItem: 'center',
   };
-
   return (
     <View style={[flexDirection, style]}>
       {label && <Text>{label}</Text>}
-      <TextInput
-        placeholder={placeholder}
-        editable={editable}
-        multiline={multiline}
-        numberOfLines={numberOfLines}
-        onChange={onChange}
-        onPressIn={onPressIn}
-        onChangeText={onChangeText}
-        secureTextEntry={password}
-        style={[defaultStyle.inputStyle, multiline ? defaultStyle.textArea : null,]}
-        // ios settings
-        clearButtonMode="always"
-        enablesReturnKeyAutomatically={true}
-        // android settings
-        disableFullscreenUI={true}
-        value={value}
-      />
+      <View style={[
+        defaultStyle.inputWrapFlex,
+        { backgroundColor: errText ? '#FFE8E8' : Colors.Medicle.Gray.Light },
+      ]}>
+        { !multiline && iconLeft && iconLeft}
+        <TextInput
+          style={[
+            defaultStyle.inputStyle,
+            multiline ? defaultStyle.textArea : null,
+            { backgroundColor: errText ? '#FFE8E8' : Colors.Medicle.Gray.Light },
+          ]}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          editable={editable}
+          multiline={multiline}
+          numberOfLines={numberOfLines}
+          onChange={onChange}
+          onPressIn={onPressIn}
+          onChangeText={onChangeText}
+          secureTextEntry={password}
+          value={value}
+          // ios settings
+          clearButtonMode="always"
+          enablesReturnKeyAutomatically={true}
+          // android settings
+          disableFullscreenUI={true}
+        />
+        {!multiline && iconRight && iconRight}
+      </View>
+      {errText ? (
+        <Text style={defaultStyle.errText}>{errText}</Text>
+      ) : (
+        <Text style={defaultStyle.errText}>&nbsp;</Text>
+      )}
     </View>
   );
 };
-
 export default MedicleInput;
