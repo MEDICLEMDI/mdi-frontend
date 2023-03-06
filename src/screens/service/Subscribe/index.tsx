52e7c907c708b0177e9f8eb2feb33ee44f9c38c9
MEDICLEMDI <medicle@medicle.io>
2023-03-06 16:08:14 +0900

    페이지 버그 수정

--- src/screens/service/Subscribe/index.tsx
+++ src/screens/service/Subscribe/index.tsx
@@ -24,7 +24,7 @@ export default () => {
           <FlatList
             horizontal={true}
             showsHorizontalScrollIndicator={false}
-            data={dentist}
+            data={dentist(t)}
             renderItem={({item}) => (<Text style={{paddingHorizontal: 12, paddingVertical: 4}}>{item.name}</Text>)} />
         </View>
         <View style={style.noData}>
