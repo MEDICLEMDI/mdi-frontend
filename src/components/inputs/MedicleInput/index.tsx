21b4c8671407fe0331ef3c65fe96e5576a759d62
MEDICLEMDI <medicle@medicle.io>
2023-03-28 18:47:39 +0900

    로그인

--- src/components/inputs/MedicleInput/index.tsx
+++ src/components/inputs/MedicleInput/index.tsx
@@ -24,128 +24,136 @@ interface TopLabelInputProps extends TextInputProps {
   textInputStyle?: StyleProp<ViewStyle>;
   clearButton?: boolean;
 }
-const MedicleInput = ({
-  direction = 'column',
-  label,
-  style,
-  placeholder,
-  editable,
-  multiline,
-  numberOfLines,
-  onChange,
-  onPressIn,
-  password,
-  onChangeText,
-  value,
-  errText,
-  maxLength,
-  leftInputNode,
-  rightInputNode,
-  inputButtonNode,
-  textInputStyle,
-  clearButton,
-  textAlign,
-  onBlur,
-}: TopLabelInputProps) => {
-  const ERROR_TEXT = fontStyleCreator({
-    color: '#FF2D2D',
-    size: 12,
-    weight: 'normal',
-  });
-  const ERROR_INPUT_BACKGROUND = '#FFE8E8';
-  const INPUT_BACKGROUND = Colors.Medicle.Gray.Light;
+const MedicleInput = React.forwardRef<TextInput, TopLabelInputProps>(
+  (
+    {
+      direction = 'column',
+      label,
+      style,
+      placeholder,
+      editable,
+      multiline,
+      numberOfLines,
+      onChange,
+      onPressIn,
+      password,
+      onChangeText,
+      value,
+      errText,
+      maxLength,
+      leftInputNode,
+      rightInputNode,
+      inputButtonNode,
+      textInputStyle,
+      clearButton,
+      textAlign,
+      onBlur,
+    },
+    ref
+  ) => {
+    const ERROR_TEXT = fontStyleCreator({
+      color: '#FF2D2D',
+      size: 12,
+      weight: 'normal',
+    });
+    const ERROR_INPUT_BACKGROUND = '#FFE8E8';
+    const INPUT_BACKGROUND = Colors.Medicle.Gray.Light;
 
-  const [errorMargin, setErrorMargin] = React.useState(0);
-  const flexStyle: ViewStyle = {
-    flexDirection: direction,
-    alignItems: direction === 'row' ? 'center' : 'baseline',
-  };
+    const [errorMargin, setErrorMargin] = React.useState(0);
+    const flexStyle: ViewStyle = {
+      flexDirection: direction,
+      alignItems: direction === 'row' ? 'center' : 'baseline',
+    };
 
-  const onLabelLayout = event => {
-    const { width } = event.nativeEvent.layout;
-    setErrorMargin(width);
-  };
+    const onLabelLayout = event => {
+      const { width } = event.nativeEvent.layout;
+      setErrorMargin(width);
+    };
 
-  const backgroundColorListener = () => {
-    if (errText) {
-      return ERROR_INPUT_BACKGROUND;
-    }
-    if (textInputStyle?.backgroundColor) {
-      if (textInputStyle[0] !== undefined) {
-        return textInputStyle.forEach(value => {
-          if (value.backgroundColor !== undefined) {
-            return value.backgroundColor;
-          }
-        });
-      } else {
-        return textInputStyle?.backgroundColor;
+    const backgroundColorListener = () => {
+      if (errText) {
+        return ERROR_INPUT_BACKGROUND;
       }
-    }
-    return INPUT_BACKGROUND;
-  };
+      if (textInputStyle?.backgroundColor) {
+        if (textInputStyle[0] !== undefined) {
+          return textInputStyle.forEach(value => {
+            if (value.backgroundColor !== undefined) {
+              return value.backgroundColor;
+            }
+          });
+        } else {
+          return textInputStyle?.backgroundColor;
+        }
+      }
+      return INPUT_BACKGROUND;
+    };
 
-  return (
-    <View style={style}>
-      <View style={[flexStyle, defaultStyle.inputWrap]}>
-        {label && (
+    return (
+      <View style={style}>
+        <View style={[flexStyle, defaultStyle.inputWrap]}>
+          {label && (
+            <View
+              onLayout={onLabelLayout}
+              style={
+                direction === 'row'
+                  ? { paddingRight: 10 }
+                  : { marginBottom: 10 }
+              }>
+              {label}
+            </View>
+          )}
           <View
-            onLayout={onLabelLayout}
-            style={
-              direction === 'row' ? { paddingRight: 10 } : { marginBottom: 10 }
-            }>
-            {label}
-          </View>
-        )}
-        <View
-          style={[
-            textInputStyle,
-            defaultStyle.inputContainer,
-            direction === 'row' && defaultStyle.inputRowDirection,
-            multiline && defaultStyle.textArea,
-            { backgroundColor: backgroundColorListener() },
-          ]}>
-          {!multiline && leftInputNode && <View>{leftInputNode}</View>}
-          <TextInput
             style={[
-              defaultStyle.input,
-              direction === 'row' && { flex: 1 },
-              multiline ? defaultStyle.textArea : null,
-              textAlign && { textAlign: textAlign },
-            ]}
-            placeholder={placeholder}
-            maxLength={maxLength}
-            editable={editable}
-            multiline={multiline}
-            numberOfLines={numberOfLines}
-            onChange={onChange}
-            onPressIn={onPressIn}
-            onChangeText={onChangeText}
-            onBlur={onBlur}
-            secureTextEntry={password}
-            value={value}
-            autoCapitalize='none'
-            // ios settings
-            clearButtonMode={clearButton === false ? 'never' : 'always'}
-            enablesReturnKeyAutomatically={true}
-            // android settings
-            disableFullscreenUI={true}
-          />
-          {!multiline && rightInputNode && <View>{rightInputNode}</View>}
+              textInputStyle,
+              defaultStyle.inputContainer,
+              direction === 'row' && defaultStyle.inputRowDirection,
+              multiline && defaultStyle.textArea,
+              { backgroundColor: backgroundColorListener() },
+            ]}>
+            {!multiline && leftInputNode && <View>{leftInputNode}</View>}
+            <TextInput
+              ref={ref}
+              style={[
+                defaultStyle.input,
+                direction === 'row' && { flex: 1 },
+                multiline ? defaultStyle.textArea : null,
+                textAlign && { textAlign: textAlign },
+              ]}
+              placeholder={placeholder}
+              maxLength={maxLength}
+              editable={editable}
+              multiline={multiline}
+              numberOfLines={numberOfLines}
+              onChange={onChange}
+              onPressIn={onPressIn}
+              onChangeText={onChangeText}
+              onBlur={onBlur}
+              secureTextEntry={password}
+              value={value}
+              autoCapitalize="none"
+              // ios settings
+              clearButtonMode={clearButton === false ? 'never' : 'always'}
+              enablesReturnKeyAutomatically={true}
+              // android settings
+              disableFullscreenUI={true}
+            />
+            {!multiline && rightInputNode && <View>{rightInputNode}</View>}
+          </View>
+          {!multiline && inputButtonNode && (
+            <View style={{ marginLeft: 10 }}>{inputButtonNode}</View>
+          )}
         </View>
-        {!multiline && inputButtonNode && (
-          <View style={{ marginLeft: 10 }}>{inputButtonNode}</View>
+        {errText && (
+          <Text
+            style={[
+              ERROR_TEXT,
+              direction === 'row' && { marginLeft: errorMargin },
+            ]}>
+            {errText}
+          </Text>
         )}
       </View>
-      {errText && (
-        <Text
-          style={[
-            ERROR_TEXT,
-            direction === 'row' && { marginLeft: errorMargin },
-          ]}>
-          {errText}
-        </Text>
-      )}
-    </View>
-  );
-};
+    );
+  }
+);
 export default MedicleInput;
